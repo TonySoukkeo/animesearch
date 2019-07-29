11f4590efe90cd8c5d8b2a8d927d12635ab6a08f
Tony Soukkeo <TonySoukkeo@Tonys-MacBook-Pro.local>
2019-07-29 00:46:43 -0600

    Updates

---
+++ precache-manifest.4e9eae102392932ab2fd2c5fba51ec7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263663d44c956bd2cbffa2a4e7ebf45f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43e1f40fb4acfd9e228e",
+    "url": "./static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "ff3b7370ab6861b3feb2",
+    "url": "./static/css/main.ccdac190.chunk.css"
+  },
+  {
+    "revision": "43e1f40fb4acfd9e228e",
+    "url": "./static/js/2.8ae4d584.chunk.js"
+  },
+  {
+    "revision": "ff3b7370ab6861b3feb2",
+    "url": "./static/js/main.24f0ce26.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
